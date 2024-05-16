955fafa45661b9b10f4b0cbab4d29d96a19ef79d
BeatrizSants <165945167+BeatrizSants@users.noreply.github.com>
2024-05-15 22:09:45 -0300

    Add files via upload

--- arcaica/home/home.js
+++ arcaica/home/home.js
@@ -1,5 +1,6 @@
-//pop-up
+//pop-up include
 w3.includeHTML();
+
 //troca background
 document.addEventListener("DOMContentLoaded", function () {
   const background = document.getElementById("background");
@@ -23,13 +24,37 @@ function showpopup(id) {
   element.style.display = "flex"; /*popup aparece */
 
   element.classList.add("show-popup");
+  if (id === "popupLog") {
+    var b1 = document.getElementById("b1");
+    var b2 = document.getElementById("b2");
+    if (b1) {
+      b1.style.display = "block";
+      b1.style.pointerEvents = "auto"; // Make interactive
+    }
+    if (b2) {
+      b2.style.pointerEvents = "auto"; // Make interactive
+    }
+  }
 }
 function hidepopup(id) {
   var element = document.getElementById(id);
   element.classList.remove("show-popup");
+
+  if (id === "popupLog") {
+    var b1 = document.getElementById("b1");
+    var b2 = document.getElementById("b2");
+    if (b1) {
+      b1.style.display = "none";
+      b1.style.pointerEvents = "none"; // Make non-interactive
+    }
+    if (b2) {
+      b2.style.display = "none";
+      b2.style.pointerEvents = "none"; // Make non-interactive
+    }
+  }
 }
 
-//onclik games
+//onclik game page
 function opengames() {
   window.location.href = "../game_selection/game_selection.html";
 }
@@ -42,4 +67,11 @@ function register(id) {
 function login(id) {
   document.getElementById("b2").style.display = "none";
   showpopup(id);
+}
+
+//turn on/off
+
+function toggleEfect(button) {
+  let turn = button.closest(".turn");
+  turn.classList.toggle("off"); //off é adicionado a turn ou removido se já estiver presente
 }
