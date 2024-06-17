1e525cfb47fa1c80c646d655632f2fb142877d60
Elyaquim <elyaquimfarias@gmail.com>
2024-06-16 23:56:25 -0300

    planejamento futuro

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const router = express.Router();
 const porta = 3000;
 
+// ========================== ROTAS GET - Páginas  =========================== 
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/home/home.html'));
 });
@@ -24,9 +25,19 @@ router.get('/settings',function(req,res){
     res.sendFile(path.join(__dirname+'/configuracoes/configuracoes.html'));
 });
 
+//TODO: FAZER ENDPOINTS DE LOGIN!
 
+//TODO: Dentre vários, outros, os seguintes enpoints:
+// // ======================== GET - Endpoint para dados  ======================= 
+// router.get('/recupera_pontuacao',function(req,res){
+//     console.log('TODO: Implementar conexão com banco e buscar pontuação. Retornar um json!');
+// });
+// // ======================= POST - Endpoint para dados  ======================= 
+// router.post('/salva_pontuacao',function(req,res){
+//     console.log('TODO: Implementar conexão com banco e salvar pontuação. Retornar status 200 se ok.');
+// });
 
 app.use('/',router);
 app.use(express.static(__dirname + '/public'));
 app.listen(process.env.port || porta);
-console.log(`Rodando servidor na porta ${porta}`);
+console.log(`Rodando servidor na porta ${porta}... Acesse em http://127.0.0.1:3000/`);
